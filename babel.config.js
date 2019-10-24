32176bcd596e6b8762c3d804564b858998fdefd7
Mateusz Chrzonstowski <mateusz.chrzonstowski@allegro.pl>
2019-10-25 01:07:38 +0200

    Add beta version of switch web component

---
+++ babel.config.js
@@ -0,0 +1,5 @@
+module.exports = (api) => api.env('test') ? {
+    plugins: [
+        '@babel/plugin-transform-modules-commonjs'
+    ]
+} : {};
---
+++ packages/switch/index.js
@@ -0,0 +1,3 @@
+import Switch from './component';
+
+Switch.register();
