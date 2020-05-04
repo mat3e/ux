bf5e3aab19c568d3ebba9196f23a750e286e3837
Mateusz Chrzonstowski <mateusz@brightcube.eu>
2020-05-04 02:55:41 +0200

    Support ARIA & forms in m3-switch

--- packages/switch/component.js
+++ packages/switch/component.js
@@ -3,9 +3,7 @@ class CheckboxWrapper extends HTMLElement {
         super();
         this.input = document.createElement('input');
         this.input.type = 'checkbox';
-        if (this.hasAttribute('name')) {
-            this.input.name = this.getAttribute('name');
-        }
+        this.input.style.display = 'none';
     }
 
     get checked() {
@@ -17,6 +15,7 @@ class CheckboxWrapper extends HTMLElement {
         if (bool === this.checked) return;
         this.input.checked = bool;
         bool ? this.setAttribute('checked', '') : this.removeAttribute('checked');
+        this.setAttribute('aria-checked', bool.toString());
         this.dispatchEvent(new Event('change', {bubbles: true}));
     }
 
@@ -24,9 +23,71 @@ class CheckboxWrapper extends HTMLElement {
         // when binding to attribute, we can get 'false' (e.g. React binds to attributes)
         return this.hasAttribute(attr) && this.getAttribute(attr) !== 'false';
     }
+
+    connectedCallback() {
+        const changeState = (event) => {
+            console.log(event.target);
+            event.preventDefault();
+            this.checked = !this.checked;
+        }
+        // Firefox - click propagates
+        // Chrome - click = just component
+        this.addEventListener('click', changeState);
+        this.addEventListener('keydown', event => {
+            if (event.code.toLowerCase() === 'space' || event.keyCode === 32) {
+                changeState(event);
+            }
+        });
+        setupLabelClickFor(this);
+        if (this.hasAttribute('name')) {
+            this.input.name = this.getAttribute('name');
+        }
+        if (this.hasAttribute('value')) {
+            this.input.value = this.getAttribute('value');
+        }
+        this.appendChild(this.input);
+        this.input.checked = this.checked;
+        this.setAttribute('aria-checked', this.checked.toString())
+        if (!this.hasAttribute('role')) {
+            this.setAttribute('role', 'checkbox');
+        }
+        if (!this.hasAttribute('tabindex')) {
+            this.setAttribute('tabindex', '0');
+        }
+    }
+}
+
+function setupLabelClickFor(component) {
+    if (component.parentElement && component.parentElement instanceof HTMLLabelElement) {
+        component.input.addEventListener('change', (event) => {
+            event.preventDefault();
+            event.stopPropagation();
+        });
+        // Firefox - clicks inner input when clicking its label
+        // Chrome - clicks component when clicking its label
+        component.input.addEventListener('click', (event) => {
+            event.preventDefault();
+            event.stopPropagation();
+            component.checked = !component.checked;
+        });
+    }
+    const id = component.hasAttribute('id') ? component.getAttribute('id') : component.id;
+    const label = (id && document.querySelector(`label[for="${id}"]`))
+        || (component.hasAttribute('aria-labelledby') && document.getElementById(component.getAttribute('aria-labelledby')));
+    if (label) {
+        label.addEventListener('click', (event) => {
+            // Chrome was clicking twice when label[for]
+            event.preventDefault();
+            component.click()
+        });
+    }
 }
 
 export default class Switch extends CheckboxWrapper {
+    static get formAssociated() {
+        return true;
+    }
+
     static register() {
         customElements.define('m3-switch', Switch);
     }
@@ -37,16 +98,11 @@ export default class Switch extends CheckboxWrapper {
     }
 
     connectedCallback() {
-        this.input.checked = this.checked;
+        super.connectedCallback();
         this.shadowRoot.innerHTML = `
             <div class="slider${this.round ? ' round' : ''}"></div>
-            <style>:host{position:relative;display:inline-block;width:2em;height:1em}input{display:none}.slider{position:absolute;cursor:pointer;top:0;left:0;right:0;bottom:0;background-color:#333333;transition:0.4s}.slider::before{position:absolute;content:"";height:calc(1em - 4px);width:calc(1em - 4px);left:2px;bottom:2px;background-color:white;transition:0.4s}input:checked+.slider{background-color:var(--m3-switch-color, #4fd69c)}input:focus+.slider{box-shadow:0 0 1px var(--m3-switch-color, #4fd69c)}input:checked+.slider::before{transform:translateX(1em)}.slider.round{border-radius:34px}.slider.round::before{border-radius:50%}</style>
+            <style>:host{position:relative;display:inline-block;width:2em;height:1em}.slider{position:absolute;cursor:pointer;top:0;left:0;right:0;bottom:0;background-color:#333333;transition:0.4s}.slider::before{position:absolute;content:"";height:calc(1em - 4px);width:calc(1em - 4px);left:2px;bottom:2px;background-color:white;transition:0.4s}:host([aria-checked="true"])>.slider{background-color:var(--m3-switch-color, #4fd69c)}:host([aria-checked="true"])>.slider::before{transform:translateX(1em)}.slider.round{border-radius:34px}.slider.round::before{border-radius:50%}</style>
         `;
-        this.shadowRoot.prepend(this.input);
-        this.onclick = (e) => {
-            e.preventDefault();
-            this.checked = !this.checked;
-        }
     }
 
     get round() {
--- packages/switch/component.spec.js
+++ packages/switch/component.spec.js
@@ -17,6 +17,38 @@ describe('m3-switch', () => {
         expect(toTest.checked).toBe(true);
     });
 
+    it('should be navigation and aria friendly', () => {
+        // given
+        document.body.innerHTML = `
+            <m3-switch checked></m3-switch>
+        `;
+
+        // when
+        const toTest = document.querySelector('m3-switch');
+
+        // then
+        expect(toTest.getAttribute('tabindex')).toBe('0');
+        expect(toTest.getAttribute('role')).toBe('checkbox');
+        expect(toTest.getAttribute('aria-checked')).toBe('true');
+    });
+
+    it('should work with form', () => {
+        // given
+        document.body.innerHTML = `
+            <form>
+                <m3-switch name="switch" value="super" checked></m3-switch>
+            </form>
+        `;
+
+        // when
+        // TODO: happy-dom doesn't support named elements
+        const toTest = document.querySelector('form').elements[0];
+
+        // then
+        expect(toTest).toHaveProperty('name', 'switch');
+        expect(toTest).toHaveProperty('value', 'super');
+    });
+
     it('should change after clicking', () => {
         // given
         document.body.innerHTML = `
@@ -35,6 +67,7 @@ describe('m3-switch', () => {
         // then
         expect(toTest.checked).toBe(false);
         expect(toTest.hasAttribute('checked')).toBe(false);
+        expect(toTest.getAttribute('aria-checked')).toBe('false');
     });
 
     it('should emit after change', () => {
@@ -55,4 +88,83 @@ describe('m3-switch', () => {
         // then
         expect(callback.mock.calls.length).toBe(2);
     });
+
+    it('should change after its label is clicked', () => {
+        // TODO: happy-dom workarounds for unsupported selectors and classes
+        const [label2, label3] = setupUglyWorkarounds();
+
+        // given
+        document.body.innerHTML = `
+            <label id="first">
+                <m3-switch id="foo" checked></m3-switch> test
+            </label>
+            <m3-switch id="other-m3"></m3-switch>
+            <m3-switch aria-labelledby="third" id="bar"></m3-switch>
+        `;
+        document.append(label2, label3)
+        const statesBefore = [...document.querySelectorAll('m3-switch')]
+            .map(m3 => m3.checked);
+
+        // when
+        document.querySelectorAll('label')
+            .forEach(label => label.click());
+
+        // then
+        const switches = document.querySelectorAll('m3-switch');
+        expect(switches[0].checked).not.toBe(statesBefore[0]);
+        expect(switches[1].checked).not.toBe(statesBefore[1]);
+        expect(switches[2].checked).not.toBe(statesBefore[2]);
+
+        // when
+        document.getElementById('foo').click();
+
+        // then
+        expect(switches[0].checked).toBe(statesBefore[0]);
+    });
 });
+
+class HTMLLabelElement extends HTMLElement {
+    click() {
+        const e = new Event('click');
+        e.target = this;
+        this.dispatchEvent(e);
+        this.children.forEach(element => element.click());
+    }
+}
+
+function setupUglyWorkarounds() {
+    global.HTMLLabelElement = HTMLLabelElement;
+    Object.defineProperty(Element.prototype, 'parentElement', {
+        get() {
+            return this.parentNode;
+        }
+    });
+    const originalGetElementClass = document.getElementClass;
+    document.getElementClass = function (tag) {
+        if (tag === 'label') {
+            return HTMLLabelElement;
+        }
+        return originalGetElementClass.apply(this, arguments);
+    }
+    // <label id="second" for="other-m3">test 2</label>
+    const label2 = document.createElement('label');
+    label2.setAttribute('for', 'other-m3');
+    label2.append('test 2');
+    // <label id="third">test 3</label>
+    const label3 = document.createElement('label');
+    label3.append('test 3');
+    const originalQuerySelector = document.querySelector;
+    document.querySelector = function (selector) {
+        if (selector === 'label[for="other-m3"]') {
+            return this.getElementById('second');
+        }
+        if (selector === '[id="second"]') {
+            return label2;
+        }
+        if (selector === '[id="third"]') {
+            return label3;
+        }
+        return originalQuerySelector.apply(this, arguments);
+    }
+    return [label2, label3];
+}
